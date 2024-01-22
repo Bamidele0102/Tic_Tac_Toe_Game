69ba7520f147ab346cb220bf94f88d1924863b7a
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-01-22 21:01:11 +0100

    Adding more documentation

--- tic_tac_toe_gui.py
+++ tic_tac_toe_gui.py
@@ -2,8 +2,15 @@ import tkinter as tk
 from tkinter import messagebox
 from tic_tac_toe import display_board, player_input, place_marker, win_check, choose_first, space_check, full_board_check
 
+app = tk.Tk()
+app.title('Tic Tac Toe')
+
+buttons = [None] + [tk.Button(app, text=' ', font=('Helvetica', 24), width=4, height=2) for _ in range(9)]
 
 def clear_board():
+    """
+    Clears the Tic Tac Toe board, resets game state, and displays the new board.
+    """
     global theBoard, player1_marker, player2_marker, turn
     theBoard = [' '] * 10
     player1_marker, player2_marker = player_input()
@@ -11,6 +18,12 @@ def clear_board():
     display_board()
 
 def on_click(position):
+    """
+    Handles button click events on the Tic Tac Toe board.
+
+    Parameters:
+    - position (int): The position (1-9) on the board where the player clicked.
+    """
     global turn
     if space_check(theBoard, position):
         if turn == 'Player 1':
@@ -37,6 +50,9 @@ app.title('Tic Tac Toe')
 buttons = [None] + [tk.Button(app, text=' ', font=('Helvetica', 24), width=4, height=2, command=lambda pos=pos: on_click(pos)) for pos in range(1, 10)]
 
 def display_board():
+    """
+    Updates the GUI to reflect the current state of the Tic Tac Toe board.
+    """
     for pos, button in enumerate(buttons[1:], start=1):
         button['text'] = theBoard[pos]
 
